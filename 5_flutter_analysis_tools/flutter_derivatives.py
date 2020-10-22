3227161b43254e1973c8eafa446bf94588e7d99e
mtnzguillermo <mtnzguillermo@gmail.com>
2020-10-22 15:29:43 +0200

    Module restructure

--- 5_flutter_analysis_tools/flutter_derivatives.py
+++ 5_flutter_analysis_tools/flutter_derivatives.py
@@ -20,6 +20,8 @@ class FlutterDerivatives():
         self.reset_all_derivatives()
 
 
+    ########## USER METHODS
+
     def reset_all_derivatives(self):
 
         # Dictionary to store all derivative values, in all notations
@@ -62,59 +64,11 @@ class FlutterDerivatives():
         pass
 
     
-    def _check_derivative_name(self, deriv):
-
-        # Listing all available derivative names
-        real_deriv_names = list(self.flutter_deriv['real'].keys())
-        complex_deriv_names = list(self.flutter_deriv['complex'].keys())
-        
-        # Raise exception if the input is not among the available ones
-        if deriv not in real_deriv_names and deriv not in complex_deriv_names:
-            msg = 'Derivative name not recognised. '
-            msg += 'It should be one of the following:\n'
-            msg += str(real_deriv_names+complex_deriv_names)
-            raise Exception(msg)
-
-
-    def _check_notation(self, notation):
-        
-        # Raise exception if the input is not among the available ones
-        if notation not in self.available_notations:
-            msg = 'The requested notation is not among the available ones.'
-            msg += ' Please select one of these: ' + str(self.available_notations)
-            raise Exception(msg)
-
-    
-    def change_default_notation(self, new_notation):
+    def set_default_notation(self, new_notation):
         
         # Change output default notation
         self._check_notation(new_notation)
         self.default_notation = new_notation
-
-
-    def get_all_derivatives(self, notation=None):
-
-        # Use default notation if none was provied
-        if notation == None:
-            notation = self.default_notation
-        # If not, check the input notation
-        else:
-            self._check_notation(notation)
-        
-        # Return a dictionary with all the derivative data
-        return self.flutter_deriv[notation]
-
-
-    def get_derivative(self, deriv):
-
-        # Check if input is among the available ones
-        self._check_derivative_name(deriv)
-        
-        # Return a small dictionary with only data from one derivative
-        if deriv in self.flutter_deriv['real'].keys():
-            return self.flutter_deriv['real'][deriv]
-        elif deriv in self.flutter_deriv['complex'].keys():
-            return self.flutter_deriv['complex'][deriv]
 
     
     def set_default_parameters(self, U=None, B=None, delta_t=None, air_dens=None):
@@ -152,6 +106,169 @@ class FlutterDerivatives():
         # Calculating derivatives pair by pair (with the motion and one force)
         for provided_force in provided_forces:
             self._calculate_derivative_pair_from_forced_motion(sim_params, provided_motion, provided_force, kwargs)
+
+
+    def get_all_derivatives(self, notation=None):
+
+        # Use default notation if none was provied
+        if notation == None:
+            notation = self.default_notation
+        # If not, check the input notation
+        else:
+            self._check_notation(notation)
+        
+        # Return a dictionary with all the derivative data
+        return self.flutter_deriv[notation]
+
+
+    def get_derivative(self, deriv):
+
+        # Check if input is among the available ones
+        self._check_derivative_name(deriv)
+        
+        # Return a small dictionary with only data from one derivative
+        if deriv in self.flutter_deriv['real'].keys():
+            return self.flutter_deriv['real'][deriv]
+        elif deriv in self.flutter_deriv['complex'].keys():
+            return self.flutter_deriv['complex'][deriv]
+
+
+    ##### INTERNAL METHODS
+
+    def _check_derivative_name(self, deriv):
+
+        # Listing all available derivative names
+        real_deriv_names = list(self.flutter_deriv['real'].keys())
+        complex_deriv_names = list(self.flutter_deriv['complex'].keys())
+        
+        # Raise exception if the input is not among the available ones
+        if deriv not in real_deriv_names and deriv not in complex_deriv_names:
+            msg = 'Derivative name not recognised. '
+            msg += 'It should be one of the following:\n'
+            msg += str(real_deriv_names+complex_deriv_names)
+            raise Exception(msg)
+
+
+    def _check_notation(self, notation):
+        
+        # Raise exception if the input is not among the available ones
+        if notation not in self.available_notations:
+            msg = 'The requested notation is not among the available ones.'
+            msg += ' Please select one of these: ' + str(self.available_notations)
+            raise Exception(msg)
+
+
+    def _check_motion_force_input(self, **kwargs):
+
+        # Check that only one motion is provided
+        motion_names = {'heave', 'pitch', 'sway'}
+        provided_motion = motion_names.intersection(kwargs)
+        if len(provided_motion) == 0:
+            msg = 'No motion time series provided. '
+            msg += 'Please provide one of the following variables: '
+            msg += str(motion_names)
+            raise Exception(msg)
+        elif len(provided_motion) > 1:
+            # TODO: This 'elif' would need to be suppressed if the function
+            # is adapted to calculate from multi-direction simulations
+            msg = 'Too many motion time series provided. '
+            msg += 'Please provide only the excited motion time series.'
+            raise Exception(msg)
+
+        # Check that at least one force is provided
+        force_names = {'lift', 'moment', 'drag'}
+        provided_forces = force_names.intersection(kwargs)
+        if len(provided_forces) == 0:
+            msg = 'No force time series provided. '
+            msg += 'Please provide at least one of the following variables: '
+            msg += str(force_names)
+            raise Exception(msg)
+
+        # TODO: check that all time series have the same length
+
+        return list(provided_motion)[0], list(provided_forces)
+
+
+    def _check_frequency_input(self, **kwargs):
+        
+        # Inputs in **kwargs that would give frequency data
+        possible_frequency_inputs = {'omega', 'frequency'}
+        provided_inputs = possible_frequency_inputs.intersection(kwargs)
+
+        # If only one is given, calculate omega (the angular frequency),
+        # which will be the one used later.
+        if len(provided_inputs) == 1:
+            provided_input = list(provided_inputs)[0]
+            if provided_input == 'omega':
+                omega = kwargs['omega']
+            elif provided_input == 'frequency':
+                omega = kwargs['frequency']*2*np.pi
+
+        # If none was given, raise exception and ask for frequency data
+        elif len(provided_inputs) == 0:
+            msg = 'No motion frequency was provided. '
+            msg += 'The frequency will be estimated from the motion time series instead. '
+            msg += 'However, the following results may be compromised, '
+            msg += 'even if the motion frequency is below the nyquist frequency.'
+            warnings.warn(msg)
+            omega = None
+        
+        # If more than one were given, raise exception
+        # to avoid incoherences between them
+        else:
+            msg = 'More than one frequency inputs were given. '
+            msg += 'In order to avoid contradictions provide '
+            msg += 'only one of the following inputs: '
+            msg += str(possible_frequency_inputs)
+            raise Exception(msg)
+        
+        # Return the omega value, it will be used later
+        return omega
+
+
+    def _fill_with_default_simulation_parameters(self, **kwargs):
+        
+        # Preparation of the error message
+        msg = 'The variable "{}" has no default value. It is necessary '
+        msg += 'to provide a particular one when calling the function.'
+
+        # Check if there is a provided value
+        # If not, check if there is at least a default value
+        if 'U' in kwargs:
+            U = kwargs['U']
+        else:
+            if getattr(self, 'U', None) != None:
+                U = self.U
+            else:
+                raise Exception(msg.format('U'))
+
+        if 'B' in kwargs:
+            B = kwargs['B']
+        else:
+            if getattr(self, 'B', None) != None:
+                B = self.B
+            else:
+                raise Exception(msg.format('B'))
+
+        if 'delta_t' in kwargs:
+            delta_t = kwargs['delta_t']
+        else:
+            if getattr(self, 'delta_t', None) != None:
+                delta_t = self.delta_t
+            else:
+                raise Exception(msg.format('delta_t'))
+
+        if 'air_dens' in kwargs:
+            air_dens = kwargs['air_dens']
+        else:
+            if getattr(self, 'air_dens', None) != None:
+                air_dens = self.air_dens
+            else:
+                raise Exception(msg.format('air_dens'))
+        
+        # Return input parameters filled with default values when necessary
+        sim_params = {'U':U, 'B':B, 'delta_t':delta_t, 'air_dens':air_dens}
+        return(sim_params)
         
 
     def _calculate_derivative_pair_from_forced_motion(self, sim_params, m_name, f_name, data):
@@ -233,119 +350,6 @@ class FlutterDerivatives():
         self.flutter_deriv['complex'][derivs_to_calc['complex']]['k'].append(k)
 
 
-    def _fill_with_default_simulation_parameters(self, **kwargs):
-        
-        # Preparation of the error message
-        msg = 'The variable "{}" has no default value. It is necessary '
-        msg += 'to provide a particular one when calling the function.'
-
-        # Check if there is a provided value
-        # If not, check if there is at least a default value
-        if 'U' in kwargs:
-            U = kwargs['U']
-        else:
-            if getattr(self, 'U', None) != None:
-                U = self.U
-            else:
-                raise Exception(msg.format('U'))
-
-        if 'B' in kwargs:
-            B = kwargs['B']
-        else:
-            if getattr(self, 'B', None) != None:
-                B = self.B
-            else:
-                raise Exception(msg.format('B'))
-
-        if 'delta_t' in kwargs:
-            delta_t = kwargs['delta_t']
-        else:
-            if getattr(self, 'delta_t', None) != None:
-                delta_t = self.delta_t
-            else:
-                raise Exception(msg.format('delta_t'))
-
-        if 'air_dens' in kwargs:
-            air_dens = kwargs['air_dens']
-        else:
-            if getattr(self, 'air_dens', None) != None:
-                air_dens = self.air_dens
-            else:
-                raise Exception(msg.format('air_dens'))
-        
-        # Return input parameters filled with default values when necessary
-        sim_params = {'U':U, 'B':B, 'delta_t':delta_t, 'air_dens':air_dens}
-        return(sim_params)
-
-
-    def _check_motion_force_input(self, **kwargs):
-
-        # Check that only one motion is provided
-        motion_names = {'heave', 'pitch', 'sway'}
-        provided_motion = motion_names.intersection(kwargs)
-        if len(provided_motion) == 0:
-            msg = 'No motion time series provided. '
-            msg += 'Please provide one of the following variables: '
-            msg += str(motion_names)
-            raise Exception(msg)
-        elif len(provided_motion) > 1:
-            # TODO: This 'elif' would need to be suppressed if the function
-            # is adapted to calculate from multi-direction simulations
-            msg = 'Too many motion time series provided. '
-            msg += 'Please provide only the excited motion time series.'
-            raise Exception(msg)
-
-        # Check that at least one force is provided
-        force_names = {'lift', 'moment', 'drag'}
-        provided_forces = force_names.intersection(kwargs)
-        if len(provided_forces) == 0:
-            msg = 'No force time series provided. '
-            msg += 'Please provide at least one of the following variables: '
-            msg += str(force_names)
-            raise Exception(msg)
-
-        # TODO: check that all time series have the same length
-
-        return list(provided_motion)[0], list(provided_forces)
-
-
-    def _check_frequency_input(self, **kwargs):
-        
-        # Inputs in **kwargs that would give frequency data
-        possible_frequency_inputs = {'omega', 'frequency'}
-        provided_inputs = possible_frequency_inputs.intersection(kwargs)
-
-        # If only one is given, calculate omega (the angular frequency),
-        # which will be the one used later.
-        if len(provided_inputs) == 1:
-            provided_input = list(provided_inputs)[0]
-            if provided_input == 'omega':
-                omega = kwargs['omega']
-            elif provided_input == 'frequency':
-                omega = kwargs['frequency']*2*np.pi
-
-        # If none was given, raise exception and ask for frequency data
-        elif len(provided_inputs) == 0:
-            msg = 'No motion frequency was provided. '
-            msg += 'The frequency will be estimated from the motion time series instead. '
-            msg += 'However, the following results may be compromised, '
-            msg += 'even if the motion frequency is below the nyquist frequency.'
-            warnings.warn(msg)
-            omega = None
-        
-        # If more than one were given, raise exception
-        # to avoid incoherences between them
-        else:
-            msg = 'More than one frequency inputs were given. '
-            msg += 'In order to avoid contradictions provide '
-            msg += 'only one of the following inputs: '
-            msg += str(possible_frequency_inputs)
-            raise Exception(msg)
-        
-        # Return the omega value, it will be used later
-        return omega
-
-
     def _get_derivatives_to_calculate(self, m_name, f_name):
         # Each combination between motion (heave, pitch, sway) and
         # force (lift, moment, drag) has some derivatives associated
@@ -379,61 +383,5 @@ class FlutterDerivatives():
         derivs_to_calc = {}
         derivs_to_calc['real'] = [real_letter+str(real_indexes[0]), real_letter+str(real_indexes[1])]
         derivs_to_calc['complex'] = 'c_' + complex_letter_1 + complex_letter_2
-        
+
         return derivs_to_calc
-
-            
-def complex2real_notation(fd_complex):
-
-    fd_real = {'H1':{'values':[],'U_red':[]},
-        'H2':{'values':[],'U_red':[]},
-        'H3':{'values':[],'U_red':[]},
-        'H4':{'values':[],'U_red':[]},
-        'A1':{'values':[],'U_red':[]},
-        'A2':{'values':[],'U_red':[]},
-        'A3':{'values':[],'U_red':[]},
-        'A4':{'values':[],'U_red':[]}}
-    
-    for letter_1 in 'ha':
-
-        if letter_1 == 'a':
-            factor_1 = 0.5
-            deriv_letter = 'A'
-        else:
-            factor_1 = 1
-            deriv_letter = 'H'
-
-        for letter_2 in 'ha':
-            
-            if letter_2 == 'a':
-                factor_2 = 0.5
-                deriv_numbers = [2,3]
-            else:
-                factor_2 = 1
-                deriv_numbers = [1,4]
-
-            for input_value, k in zip(fd_complex['c_'+letter_1+letter_2]['values'], fd_complex['c_'+letter_1+letter_2]['k']):
-
-                U_red = np.pi / k
-
-                output_values = np.array([np.imag(input_value), np.real(input_value)]) * np.pi * 0.5 * factor_1 * factor_2
-
-                for output_value, deriv_number in zip(output_values, deriv_numbers):
-                    fd_real[deriv_letter+str(deriv_number)]['values'].append(output_value)
-                    fd_real[deriv_letter+str(deriv_number)]['U_red'].append(U_red)
-    
-    return fd_real
-                
-
-def real2complex_notation(fd_real):
-    pass
-
-
-if __name__ == '__main__':
-    fd_complex = {'c_aa':{'values':[2.4-0.36j],'k':[0.5]},
-        'c_ah':{'values':[0.0071+1.2j],'k':[0.5]},
-        'c_ha':{'values':[-3.6-0.45j],'k':[0.5]},
-        'c_hh':{'values':[0.063-1.6j],'k':[0.5]}}
-
-    fd_real = complex2real_notation(fd_complex)
-    print(fd_real)
